6166ec7b7918a6795b0eea17717db367aa79b736
vasavishanmugam <vasavishanmugam9@gmail.com>
2026-07-17 22:16:24 +0530

    Completed Day 8 and Day 9 - DTO, Logging, Exception Handling, ResponseEntity, Auditing, ModelMapper

--- employee-service/src/main/java/com/vasavi/employee_service/exception/GlobalExceptionHandler.java
+++ employee-service/src/main/java/com/vasavi/employee_service/exception/GlobalExceptionHandler.java
@@ -3,7 +3,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
