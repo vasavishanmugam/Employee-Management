5d97c3863ea22720ce0513317c3b60e40f8e1fef
vasavishanmugam <vasavishanmugam9@gmail.com>
2026-07-29 08:44:43 +0530

    Revert "Day 36: Display Employee List in React Table"

--- employeehub-ui/src/App.jsx
+++ employeehub-ui/src/App.jsx
@@ -1,17 +1,14 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import api from './services/api';
-import "./App.css";
+
 
 function App() {
-
-  const[employees, setEmployees] = useState([]);
-
   useEffect(() =>{
     async function fetchEmployees(){
       try
       {
         const response = await api.get("/employees");
-        setEmployees(response.data.data.content);
+        console.log(response.data);
       }
       catch(error)
       {
@@ -23,31 +20,9 @@ function App() {
   }, []);
 
   return(
-    <div className="container">
-    <h1 className="title">Employee Management System</h1>
-
-    <table>
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Name</th>
-          <th>Email</th>
-          <th>Salary</th>
-        </tr>
-      </thead>
-
-      <tbody>
-        {employees.map((employee) => (
-          <tr key={employee.id}>
-            <td>{employee.id}</td>
-            <td>{employee.name}</td>
-            <td>{employee.email}</td>
-            <td>₹ {employee.salary.toLocaleString("en-IN")}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  </div>
+    <div>
+      <h1>Employee Management System</h1>
+    </div>
   );
 }
 
